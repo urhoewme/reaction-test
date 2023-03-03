3491e0bcdfe2ed52019d6d3542743a75d3229930
urhoewme <gheniavip@gmail.com>
2023-03-03 20:27:30 +0300

    modified:   public/index.html 	modified:   src/App.vue 	new file:   src/components/Alert.vue 	new file:   src/components/Block.vue 	deleted:    src/components/HelloWorld.vue 	new file:   src/components/Results.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction test</h1>
+  <button :disabled="isPlaying" @click="start">Start new game</button>
+  <Block :delay="delay" v-if="isPlaying" @end="endGame" />
+  <Results :score="score" v-if="showResults" />
+  <Alert v-if="preFire" />
+  <div class="alert-area" @click.self="togglePref"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from "@/components/Block.vue";
+import Results from "@/components/Results.vue";
+import Alert from "@/components/Alert.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Block,
+    Results,
+    Alert,
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+      preFire: false,
+    };
+  },
+  methods: {
+    start() {
+      this.isPlaying = true;
+      this.delay = 2000 + Math.random() * 5000;
+      this.showResults = false;
+      this.preFire = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    },
+    togglePref() {
+      if (this.isPlaying === true) {
+        this.preFire = true;
+        this.isPlaying = false;
+      }
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Montserrat", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: teal;
   margin-top: 60px;
+}
+
+.alert-area {
+  top: 0;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  margin-top: 200px;
 }
 </style>
